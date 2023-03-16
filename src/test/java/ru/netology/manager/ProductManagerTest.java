4d63663a62bfe0874c314b38a9d56282d734ef6e
Sergey <zhuravlevsergey7@gmail.com>
2023-03-16 22:15:43 +0300

    tests added

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -13,21 +13,28 @@ class ProductManagerTest {
     ProductRepository repo = new ProductRepository();
     ProductManager manager = new ProductManager(repo);
 
-    Product book = new Book(123, "Master", 2000, "Bernard");
-    Product smartphone = new Smartphone(741, "Iphone", 12500, "Apple");
+    Product book1 = new Book(123, "Fantasy", 2000, "Lord of rings");
+    Product book2 = new Book(258, "Fantasy", 800, "Master");
+    Product book3 = new Book(357, "Novel", 1200, "Bernard");
+
+
+    Product smartphone1 = new Smartphone(741, "Iphone", 12500, "Apple");
+    Product smartphone2 = new Smartphone(951, "Samsung", 25000, "Samsung Inc.");
 
     @BeforeEach
     public void SetUp() {
-        manager.add(book);
-        manager.add(smartphone);
-
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
     }
 
 
     @Test
     void findProductBySmartphoneName() {
 
-        Product[] expected = {smartphone};
+        Product[] expected = {smartphone1};
         Product[] actual = manager.searchBy("Iphone");
 
         Assertions.assertArrayEquals(expected, actual);
@@ -36,8 +43,24 @@ class ProductManagerTest {
 
     @Test
     void findProductByBookName() {
-        Product[] expected = {book};
-        Product[] actual = manager.searchBy("Master");
+        Product[] expected = {book3};
+        Product[] actual = manager.searchBy("Novel");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void findProductByEqualName() {
+        Product[] expected = {book1, book2};
+        Product[] actual = manager.searchBy("Fantasy");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotFindAnyProduct() {
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Fairytale");
 
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -45,8 +68,8 @@ class ProductManagerTest {
     @Test
     void removeProduct() {
 
-        repo.removeByID(123);
-        Product[] expected = {smartphone};
+        repo.removeByID(357);
+        Product[] expected = {book1, book2, smartphone1, smartphone2};
         Product[] actual = repo.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
