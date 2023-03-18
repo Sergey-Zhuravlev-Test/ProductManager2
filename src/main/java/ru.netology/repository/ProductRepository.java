ecda25c13451c830a61608a3934c8d89750e71e8
Sergey <zhuravlevsergey7@gmail.com>
2023-03-18 17:09:44 +0300

    exception #1 added

--- src/main/java/ru.netology/repository/ProductRepository.java
+++ src/main/java/ru.netology/repository/ProductRepository.java
@@ -1,37 +1,51 @@
 package ru.netology.repository;
 
 import ru.netology.domain.Product;
+import ru.netology.exceptions.NotFoundException;
 
 public class ProductRepository {
-    private Product[] items = new Product[0];
+    private Product[] products = new Product[0];
 
-    public void save(Product item) {
-        Product[] tmp = new Product[items.length + 1];
-        for (int i = 0; i < items.length; i++) {
-            tmp[i] = items[i];
+    public void save(Product product) {
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
 
         }
-        tmp[tmp.length - 1] = item;
-        items = tmp;
+        tmp[tmp.length - 1] = product;
+        products = tmp;
 
     }
 
     public Product[] findAll() {
-        return items;
+        return products;
 
     }
 
-    public void removeByID(int id) {
-        Product[] tmp = new Product[items.length - 1];
+    public void removeByID(int removeId) {
+        Product removeProduct = findById(removeId);
+        if (removeProduct == null) {
+            throw new NotFoundException(removeId);
+        }
+        Product[] tmp = new Product[products.length - 1];
         int copyToIndex = 0;
-        for (Product item : items) {
-            if (item.getId() != id) {
-                tmp[copyToIndex] = item;
+        for (Product product : products) {
+            if (product.getId() != removeId) {
+                tmp[copyToIndex] = product;
                 copyToIndex++;
             }
         }
-        items = tmp;
+        products = tmp;
 
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
+            }
+        }
+        return null;
     }
 
 
---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,50 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.exceptions.NotFoundException;
+
+
+public class ProductRepositoryTest {
+    ProductRepository repo = new ProductRepository();
+
+    Product book1 = new Book(123, "Fantasy", 2000, "Lord of rings");
+    Product book2 = new Book(258, "Fantasy", 800, "Master");
+    Product book3 = new Book(357, "Novel", 1200, "Bernard");
+
+
+    Product smartphone1 = new Smartphone(741, "Iphone", 12500, "Apple");
+    Product smartphone2 = new Smartphone(951, "Samsung", 25000, "Samsung Inc.");
+
+    @BeforeEach
+    public void SetUp() {
+        repo.save(book1);
+        repo.save(book2);
+        repo.save(book3);
+        repo.save(smartphone1);
+        repo.save(smartphone2);
+    }
+
+    @Test
+    void removeWhenProductFound() {
+
+        repo.removeByID(258);
+        Product[] expected = {book1, book3, smartphone1, smartphone2};
+        Product[] actual = repo.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void removeWhenProductNotFound() {
+        Assertions.assertThrows(NotFoundException.class, () -> repo.removeByID(555));
+
+    }
+
+}
